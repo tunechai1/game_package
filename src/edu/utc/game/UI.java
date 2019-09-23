8938aa3d4c5f25892a82e8bf90ae65ab7d4fef80
Craig Tanis <craig-tanis@utc.edu>
2019-09-23 15:09:52 -0400

    enabled transparency

--- src/edu/utc/game/UI.java
+++ src/edu/utc/game/UI.java
@@ -47,7 +47,9 @@ public class UI {
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
         
     	GL11.glEnable(GL11.GL_TEXTURE_2D);
-        
+    	GL11.glEnable(GL11.GL_BLEND);
+    	GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
         this.width=width;
         this.height=height;
 
